9a469e637dbdb956b3b3fc96a0d4cba3e5dcf30d
Mathias Nonohay <mathias.gianotti.nonohay@sibs.com>
2023-08-24 16:35:25 +0100

    Added author work list view controller

---
+++ ReaderApp/Model/Work/Work.swift
@@ -0,0 +1,18 @@
+//
+//  Work.swift
+//  ReaderApp
+//
+//  Created by Mathias Nonohay on 24/08/2023.
+//
+
+import Foundation
+
+struct Work {
+    let title: String
+    let date: String
+    let firstEdition: String
+    let source: String
+    let content: String
+    let translation: String
+    let transcrition: String
+}
---
+++ ReaderApp/ViewController/AuthorWorkList/AuthorWorkListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AuthorWorkListViewController.swift
+//  ReaderApp
+//
+//  Created by Mathias Nonohay on 24/08/2023.
+//
+
+import UIKit
+
+class AuthorWorkListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
